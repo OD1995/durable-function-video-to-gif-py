1fbb2edc6892224317686dd1730f50130e1fd45e
Shrikrishna Khose <shkhose@microsoft.com>
2020-08-25 15:19:20 -0400

    SAS UpDATE

--- Task_GetSASUri/__init__.py
+++ Task_GetSASUri/__init__.py
@@ -10,19 +10,22 @@ import logging
 import os
 import json
 from datetime import datetime, timedelta
-from azure.storage.blob import BlobServiceClient, generate_blob_sas, BlobSasPermissions
+from azure.storage.blob import BlobServiceClient, generate_account_sas, AccountSasPermissions ,ResourceTypes
 
 def main(options: str) -> str:
     blobOptions = (json.loads(options))
     container = blobOptions['container']
     blob =  blobOptions['blob']
+    account_name= os.getenv('ACCOUNT_NAME')
+    account_key= os.getenv('ACCOUNT_KEY')
 
-    sas_token = generate_blob_sas(
-    account_name= os.getenv('ACCOUNT_NAME'),
-    account_key= os.getenv('ACCOUNT_KEY'),
-    container_name= container,
-    blob_name = blob,
-    permission=BlobSasPermissions(read=True),
+    sas_token = generate_account_sas(
+    account_name= account_name,
+    account_key= account_key,
+    resource_types=ResourceTypes(object=True),
+    # container_name= container,
+    # blob_name = blob,
+    permission=AccountSasPermissions(read=True),
     expiry=datetime.utcnow() + timedelta(hours=1)
 )
     sas_uri = f"https://ragrsemo.blob.core.windows.net/{container}/{blob}?{sas_token}"
